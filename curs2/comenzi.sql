bc2c95a93365dac618eb8e1827582d8f9abd291d
xMariuss <mariuss.b094@gmail.com>
2024-05-15 20:43:23 +0300

    dataaccess2324

--- curs2/comenzi.sql
+++ curs2/comenzi.sql
@@ -1,13 +1,29 @@
-CREATE TABLE `magazin`.`produse` 
-(`idprodus` INT UNSIGNED NOT NULL , 
-`numeprodus` VARCHAR(350) NOT NULL , 
-`cantitate` INT UNSIGNED NOT NULL , 
-`idfirma` INT UNSIGNED NOT NULL , 
-`firma` VARCHAR(250) NOT NULL , 
-`adresafirma` VARCHAR(500) NOT NULL , 
-`modelp` VARCHAR(500) NOT NULL , 
-`stocp` INT UNSIGNED NOT NULL , 
-`pret` DECIMAL(8,2) UNSIGNED NOT NULL , 
-`categorie` VARCHAR(250) NOT NULL , 
-`descrierep` TEXT NOT NULL , 
-PRIMARY KEY (`idprodus`)) ENGINE = InnoDB;
+CREATE TABLE `magazin`.`produse` (
+    `idprodus` INT UNSIGNED NOT NULL , 
+    `numeprodus` VARCHAR(350) NOT NULL , 
+    `cantitate` INT UNSIGNED NOT NULL , 
+    `idfirma` INT UNSIGNED NOT NULL , 
+    `firma` VARCHAR(250) NOT NULL , 
+    `adresafirma` VARCHAR(500) NOT NULL , 
+    `modelp` VARCHAR(500) NOT NULL , 
+    `stocp` INT UNSIGNED NOT NULL , 
+    `pret` DECIMAL(8,2) UNSIGNED NOT NULL , 
+    `categorie` VARCHAR(250) NOT NULL , 
+    `descrierep` TEXT NOT NULL , 
+    PRIMARY KEY (`idprodus`)
+) ENGINE = InnoDB;
+
+
+INSERT INTO `produse` (
+    `idprodus`, `numeprodus`, `cantitate`, `idfirma`, `firma`, `adresafirma`, `modelp`, `stocp`, `pret`, `categorie`, `descrierep`
+    ) VALUES (
+        '0', 'TV', '150', '1', 'LG', 'Adresa LG', 'LG HD', '150', '22.5', 'TV', 'Descriere produs'
+        );
+
+
+
+
+-- SELECT COUNT(id) AS nr_studenti, an FROM `student` GROUP BY an;
+SELECT COUNT(id) AS nr_studenti, an FROM `student` GROUP BY an HAVING nr_studenti >= 10 ORDER BY an DESC;
+
+SELECT SUM(bursa) AS total FROM `student` WHERE an = 1;
